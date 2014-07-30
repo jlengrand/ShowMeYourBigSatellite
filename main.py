e9c61005284140addbda443b02fc0ec3aeb55d10
Julien Lengrand-Lambert <julien@lengrand.fr>
2014-07-30 18:19:07 +0200

    Modifies main.py so it serves the html and the associated js and images. Flask is not supposed to do that, but rather nginx or any other webserver

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from flask import Flask
-app = Flask(__name__)
+import os
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/images/<path:path>')
+def static_proxy_images(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(os.path.join('images', path))
+
+@app.route('/js/<path:path>')
+def static_proxy_js(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(os.path.join('js', path))
+
+@app.route('/')
+def root():
+    return app.send_static_file('simplemap.html')
+
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
 
 if __name__ == "__main__":
     app.run()
