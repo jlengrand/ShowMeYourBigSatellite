d19bbe8f0a953ecc432c44058008308c4c94cb39
vinzdeb <vinzdeb@hotmail.com>
2014-08-01 18:30:49 +0200

    Refactoring for easier use in the future

--- whereismysat.py
+++ whereismysat.py
@@ -6,49 +6,55 @@ import datetime
 import urllib2
 
 #global
-satlist=[]
+satlist={}
 
-#
-def init(s):
-    """ Read a TLEs file stream and creates a list of satellites."""
-    global satlist
-    tmplist = []
-    l1 = s.readline()
-    while l1:
-        l2 = s.readline()
-        l3 = s.readline()
-        sat = ephem.readtle(l1,l2,l3)
-        tmplist.append(sat)
+class TleGroup:
+    """ Group of TLEs """
+    def __init__(self, name, url):
+        """save url and load TLEs"""
+        self.url = url
+        self.name = name
+        self.load()
+    
+    def load(self):
+        """ Read a TLEs file stream and creates a list of satellites.
+        This function aims to be called every day"""
+        global satlist
+        s = urllib2.urlopen(self.url)
+        count = 0
         l1 = s.readline()
-
-    s.close()
-    if len(tmplist)> 0:
-        satlist=tmplist
-        print "%i satellites loaded into list"%len(satlist)
+        while l1:
+            l2 = s.readline()
+            l3 = s.readline()
+            sat = ephem.readtle(l1,l2,l3)
+            if sat is not None:
+                #add/update satlist
+                satlist[sat.name]=sat
+                count+=1
+            l1 = s.readline()
+        s.close()
+        print "%i satellites loaded into list / Total in list: %i"%(count,len(satlist))
 
 
 def getPos(name):
-    global satList
-    for sat in satlist:
-        if sat.name == name:
-                sat.compute(datetime.datetime.utcnow())
-                print('%s  lon: %s  lat: %s' % (sat.name, sat.sublong.real / ephem.degree, sat.sublat.real / ephem.degree))
-                return(sat)
+    global satlist
+    for key in satlist.keys():
+        if key == name:
+            sat=satlist[key]
+            sat.compute(datetime.datetime.utcnow())
+            print('%s  lon: %s  lat: %s' % (sat.name, sat.sublong.real / ephem.degree, sat.sublat.real / ephem.degree))
+            return(sat)
 
 
 
 def main():
-    """ For testing """
-    #load TLEs (should be done every day)
-    #init(open("science.txt"))
-    init(urllib2.urlopen("http://www.celestrak.com/NORAD/elements/science.txt"))
 
     #get position
     getPos("TERRA")
 
 
 #when import
-init(urllib2.urlopen("http://www.celestrak.com/NORAD/elements/science.txt"))
+science = TleGroup("Space & Earth Science", "http://www.celestrak.com/NORAD/elements/science.txt")
 
 
 if __name__ == "__main__":
